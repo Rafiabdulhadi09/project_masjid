5c6c364a2aafb6103f0ff10884bce4dde0eb476f
RismaDwiSanjaya <sanjayarixmadwi@gmail.com>
2025-01-22 14:52:06 +0700

    beranda

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -52,10 +52,10 @@
                 <div class="col-md-6 animated fadeIn">
                     <div class="owl-carousel header-carousel">
                         <div class="owl-carousel-item">
-                            <img class="img-fluid" src="img/carousel-1.jpg" alt="">
+                            <img class="img-fluid" src="img/putra.jpg" alt="">
                         </div>
                         <div class="owl-carousel-item">
-                            <img class="img-fluid" src="img/carousel-2.jpg" alt="">
+                            <img class="img-fluid" src="img/jami.jpg" alt="">
                         </div>
                     </div>
                 </div>
@@ -65,39 +65,44 @@
 
 
         <!-- Search Start -->
-        <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
-            <div class="container">
-                <div class="row g-2">
-                    <div class="col-md-10">
-                        <div class="row g-2">
-                            <div class="col-md-4">
-                                <input type="text" class="form-control border-0 py-3" placeholder="Search Keyword">
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-select border-0 py-3">
-                                    <option selected>Property Type</option>
-                                    <option value="1">Property Type 1</option>
-                                    <option value="2">Property Type 2</option>
-                                    <option value="3">Property Type 3</option>
-                                </select>
-                            </div>
-                            <div class="col-md-4">
-                                <select class="form-select border-0 py-3">
-                                    <option selected>Location</option>
-                                    <option value="1">Location 1</option>
-                                    <option value="2">Location 2</option>
-                                    <option value="3">Location 3</option>
-                                </select>
-                            </div>
-                        </div>
+<div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="table-responsive">
+                    <div class="text-center mb-4 text-white">
+                        <h4 class="text-white">Jadwal Salat</h4>
+                        <p>Kecamatan Kawali, Kabupaten Ciamis, Jawa Barat 46253 · Januari 2025</p>
                     </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-dark border-0 w-100 py-3">Search</button>
-                    </div>
+                    <table class="table table-bordered text-center bg-white">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>Subuh</th>
+                                <th>Terbit</th>
+                                <th>Zuhur</th>
+                                <th>Asar</th>
+                                <th>Maghrib</th>
+                                <th>Isya</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>04.21</td>
+                                <td>05.44</td>
+                                <td>11.58</td>
+                                <td>15.21</td>
+                                <td>18.12</td>
+                                <td>19.26</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-        <!-- Search End -->
+    </div>
+</div>
+<!-- Search End -->
+
 
 
         <!-- Category Start -->
