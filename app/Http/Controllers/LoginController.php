28faa7aa6d1e47627dc13e9ae0ec2a92afef4c25
R09AH <rafiabdulh09@gmail.com>
2025-01-22 14:55:03 +0700

    login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username'=>'required',
+            'password'=>'required'
+        ],[
+            'username.required'=>'Username wajib diisi',
+            'password.required'=>'Password wajib diisi'
+        ]);
+
+        $infologin = [
+            'username'=> $request->username,
+            'password'=>$request->password
+        ];
+        if (Auth::attempt($infologin)) {
+            return redirect('/admin');
+        } else{
+            return redirect()->back()->withErrors('Username Dan Password yang di masukan salah');
+        }
+    }
+      public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/')->with('success', 'Anda berhasil logout.');
+    }
+}
